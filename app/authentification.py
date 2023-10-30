f5e995d51e9a55a8cf3f902b3d7f0cb9ce733f8b
Louarradi <mlouarra@gmail.com>
2023-10-31 00:13:10 +0100

    authentification simple

--- app/authentification.py
+++ app/authentification.py
@@ -2,9 +2,9 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from passlib.context import CryptContext
-from jwt import PyJWTError as JWTError
 import jwt
 from datetime import datetime, timedelta
+from jwt.exceptions import DecodeError
 
 SECRET_KEY = "my_secret_key"
 ALGORITHM = "HS256"
@@ -26,7 +26,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str
-    exp: datetime
+    #exp: datetime
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -36,9 +36,12 @@ def get_user(db, username: str):
         user_dict = db[username]
         return user_dict
 
-def create_access_token(data: dict):
+def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -55,11 +58,9 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
-    except JWTError:
+    except DecodeError:
         raise credentials_exception
     user = get_user(fake_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
-
-
--- app/main.py
+++ app/main.py
@@ -1,49 +1,37 @@
+from datetime import timedelta
+
 import uvicorn
-from fastapi import FastAPI, HTTPException, Depends, UploadFile, File , Form
-from pydantic import BaseModel
-import tempfile
-import shutil
-import data_query
-import data_update
-import log
-import model_update
-# from model_RESNET import predict_category as predict_category_resnet
-# from model_BERT import modele_bert, convert_prediction_to_thematique_and_code
-import numpy as np
-from model_Fusion import predict
+from fastapi import FastAPI, HTTPException, Depends, UploadFile, File, Form
 from fastapi.responses import JSONResponse
-from typing import Dict
-
-
-
+from authentification import get_user, create_access_token, get_current_user, ACCESS_TOKEN_EXPIRE_MINUTES, fake_users_db, Token, oauth2_scheme, OAuth2PasswordRequestForm, verify_password
 
 app = FastAPI(title="API RAKUTEN")
 
-# Modèles Pydantic
-class LoginData(BaseModel):
-    username: str
-    password: str
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = get_user(fake_users_db, form_data.username)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    if not verify_password(form_data.password, user["hashed_password"]):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-class MessageResponse(BaseModel):
-    message: str
-
-class TextItem(BaseModel):
-    text: str
-
-# Routes
-
-
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"username": user["username"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.post("/model/fusion/predict")
-async def predict_endpoint(text: str = Form(...), image: UploadFile = UploadFile(...)):
+async def predict_endpoint(text: str = Form(...), image: UploadFile = UploadFile(...), current_user: dict = Depends(get_current_user)):
     image_contents = await image.read()
     with open("temp_image.jpg", "wb") as f:
         f.write(image_contents)
 
-    prdtypecode, thematique = predict(text, "temp_image.jpg")
-    return JSONResponse(content={"prdtypecode": prdtypecode, "thematique": thematique})
+    # Votre code de prédiction ici...
+    # prdtypecode, thematique = predict(text, "temp_image.jpg")
+    # return JSONResponse(content={"prdtypecode": prdtypecode, "thematique": thematique})
 
+    return {"message": "Prediction placeholder"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
