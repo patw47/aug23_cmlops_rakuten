4da1f5b059f317ce40a00067bf591d020e14f352
patw47 <136432921+patw47@users.noreply.github.com>
2023-11-01 10:46:50 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ import shutil
 #import data_update
 #import log
 #import model_update
-from model_RESNET import predict_category as predict_category_resnet
-from model_BERT import modele_bert, convert_prediction_to_thematique_and_code
+#from model_RESNET import predict_category as predict_category_resnet
+#from model_BERT import modele_bert, convert_prediction_to_thematique_and_code
 import numpy as np
 from model_Fusion import predict
 from fastapi.responses import JSONResponse
@@ -32,27 +32,27 @@ class TextItem(BaseModel):
 
 # Routes
 
-@app.post("/model/resnet/predict")
-async def predict_resnet_endpoint(file: UploadFile):
-    try:
+#@app.post("/model/resnet/predict")
+#async def predict_resnet_endpoint(file: UploadFile):
+#    try:
         # Utilisez la fonction renommée ici et supprimez "await"
-        category, thematique = predict_category_resnet(file)
-        return {"category": category, "thematique": thematique}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#        category, thematique = predict_category_resnet(file)
+#        return {"category": category, "thematique": thematique}
+#    except Exception as e:
+#        raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/model/bert/predict")
-async def predict_category(item: TextItem):
-    try:
-        prediction = modele_bert(item.text)
-        prd_type_code, thematique = convert_prediction_to_thematique_and_code(prediction)
-        return {
-            "prd_type_code": prd_type_code,
-            "thematique": thematique
-        }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#@app.post("/model/bert/predict")
+#async def predict_category(item: TextItem):
+#    try:
+#        prediction = modele_bert(item.text)
+#        prd_type_code, thematique = convert_prediction_to_thematique_and_code(prediction)
+#        return {
+#            "prd_type_code": prd_type_code,
+#            "thematique": thematique
+#        }
+#    except Exception as e:
+#        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/model/fusion/predict")
